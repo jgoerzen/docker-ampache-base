0f4a694fe793f52ac5dcc03dfc0c8d4ef0eabb08
John Goerzen <jgoerzen@complete.org>
2020-10-25 23:12:03 -0500

    updates

--- setup/getcomposer.sh
+++ setup/getcomposer.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+set -e
+set -x
+
 EXPECTED_SIGNATURE=$(wget -q -O - https://composer.github.io/installer.sig)
 php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
 ACTUAL_SIGNATURE=$(php -r "echo hash_file('SHA384', 'composer-setup.php');")
@@ -11,7 +14,8 @@ then
     exit 1
 fi
 
-php composer-setup.php
+# jgoerzen 2020-10-25: force 1.0.x version due to issue with 2.x for now; remove --version= later.
+php composer-setup.php --version=1.10.16
 RESULT=$?
 rm composer-setup.php
 exit $RESULT
