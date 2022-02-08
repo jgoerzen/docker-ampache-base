212f393a97f324e29502e3673629fc3985b004e9
John Goerzen <jgoerzen@complete.org>
2022-02-07 20:36:08 -0600

    version stuff

--- setup/setup.sh
+++ setup/setup.sh
@@ -7,12 +7,15 @@ cd /tmp/setup
 /tmp/setup/getcomposer.sh
 mv -vi composer.phar /usr/local/bin/composer
 
-wget https://github.com/ampache/ampache/archive/4.2.3.tar.gz
+# WHEN UPDATING VERSION, ALSO UPDATE GITLAB CI FILE!
+VERSION=4.2.3
+
+wget https://github.com/ampache/ampache/archive/$VERSION.tar.gz
 sha256sum -c < sums
 
 cd /var/www/html
-tar -zxvf /tmp/setup/4.2.3.tar.gz
-mv ampache-4.2.3 ampache
+tar -zxvf /tmp/setup/$VERSION.tar.gz
+mv ampache-$VERSION ampache
 
 exec rm -rf /tmp/setup
 
