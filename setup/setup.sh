2532956ec2fa0438208d2ac340e3885089640f5d
John Goerzen <jgoerzen@complete.org>
2023-04-22 22:18:47 -0500

    5.6.0

--- setup/setup.sh
+++ setup/setup.sh
@@ -6,7 +6,7 @@ set -x
 cd /tmp/setup
 
 # WHEN UPDATING VERSION, ALSO UPDATE GITLAB CI FILE!
-VERSION=5.5.5
+VERSION=5.6.0
 
 wget https://github.com/ampache/ampache/releases/download/$VERSION/ampache-${VERSION}_all.zip
 sha256sum -c < sums
