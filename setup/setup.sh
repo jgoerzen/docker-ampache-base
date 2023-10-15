8ed499bf45d9fa28c37dc48186727491e96c9607
John Goerzen <jgoerzen@complete.org>
2023-10-15 08:15:23 -0500

    6.0.3

--- setup/setup.sh
+++ setup/setup.sh
@@ -6,15 +6,15 @@ set -x
 cd /tmp/setup
 
 # WHEN UPDATING VERSION, ALSO UPDATE GITLAB CI FILE!
-VERSION=5.6.2
+VERSION=6.0.3
 
-wget https://github.com/ampache/ampache/releases/download/$VERSION/ampache-${VERSION}_all.zip
+wget https://github.com/ampache/ampache/releases/download/$VERSION/ampache-${VERSION}_all_php8.2.zip
 sha256sum -c < sums
 
 cd /var/www/html
 mkdir ampache
 cd ampache
-unzip /tmp/setup/ampache-${VERSION}_all.zip
+unzip /tmp/setup/ampache-${VERSION}_all_php8.2.zip
 
 sed -i 's/^memory_limit.*$/memory_limit = 512M/' /etc/php/*/apache2/php.ini
 
